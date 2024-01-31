1904568f48d680bb610f2cdafc7456cf2f0d1e28
Ernest Czechowski <ernest.czechowski@gmail.com>
2024-01-31 16:27:36 +0100

    s

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,24 @@
 //const mqtt = require('mqtt');
 import mqtt, { MqttClient } from 'mqtt';
 
-const client = mqtt.connect('mqtt://192.168.1.82', {
-  clientId: 'IES_Gateway_Mqtt_Client',
-});
+const backendAPI_URL = 'localhost:3000/IESGateway/';
+
+const mqttConfig: mqtt.IClientOptions = {
+  clientId: 'IES_Gateway_Mqtt_client',
+};
+
+const client = mqtt.connect('localhost', mqttConfig);
 
 const topic: string = 'test';
 
 client.on('connect', () => {
   client.subscribe(topic, (err) => {
-    if (err) {
-      console.log('connection failed!' + err.message.toString());
-      return;
-    }
-
-    client.on('message', (topic: any, message: any) => {
-      console.log(message.toString());
+    client.on('message', (topic, message, packet) => {
+      //TODO send data to API
     });
   });
+});
+
+client.on('close', () => {
+  console.log('disconnected');
 });
