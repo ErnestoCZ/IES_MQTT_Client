2ae464dc5adc0cde718617bfd6689fc3907e433c
Ernest Czechowski <ernest.czechowski@gmail.com>
2024-07-05 13:02:45 +0200

    insert hardcoded topics

--- src/app.ts
+++ src/app.ts
@@ -1,57 +1,63 @@
 //const mqtt = require('mqtt');
-import mqtt from 'mqtt';
-import axios from 'axios';
-const host: string = 'localhost';
-const path: string = 'IESGateway/';
-const port: number = 3000;
+import mqtt from "mqtt";
+import axios from "axios";
+const host: string = "192.168.1.105";
+const path: string = "IESGateway/";
 const reconnectAfterMS = 10000;
-const APIUrl: string = 'http://localhost:3000/IESGateway/';
+const backendAddr: string = "localhost";
+const backendPort: number = 3002;
+const APIUrl: string = `http://${backendAddr}:${backendPort}/`;
 
 //Config
 const mqttConfig: mqtt.IClientOptions = {
-  clientId: 'IES_Gateway_Mqtt_client',
-  clean: true,
+  clientId: "IES_Gateway_MQTT_Client",
+  clean: false,
   reconnectPeriod: reconnectAfterMS,
 };
 
 //Clients
-const MQTTClient = mqtt.connect('mqtt://localhost', mqttConfig); //connects to the local Broker
+const MQTTClient = mqtt.connect(`mqtt://${host}`, mqttConfig); //connects to the local Broker
 
-const topic: string[] = ['test', 'test1', 'SensorDataToGateway'];
+//TODO FETCH AVAILABLE TOPICS via HTTP Request
+const topic: string[] = [
+  "application/82bcd164-9c50-4ff1-8dda-50b66c528472/device/5bd2ffa00648c0b2/event/up",
+  "application/82bcd164-9c50-4ff1-8dda-50b66c528472/device/5bd2ffa00648c0b5/event/up",
+  "application/82bcd164-9c50-4ff1-8dda-50b66c528472/device/5bd2ffa00648c0b3/event/up",
+];
 
-MQTTClient.on('connect', () => {
-  console.log('Connected to local MQTT-Broker');
+MQTTClient.on("connect", () => {
+  console.log("Connected to local MQTT-Broker");
   MQTTClient.subscribe(topic, (err) => {});
 });
 
-MQTTClient.on('close', () => {
-  console.log('Connection to Broker lost');
+MQTTClient.on("close", () => {
+  console.log("Connection to Broker lost");
 });
-MQTTClient.on('error', (error) => {
+MQTTClient.on("error", (error) => {
   console.log(error);
   MQTTClient.end();
 });
-MQTTClient.on('offline', () => {
+MQTTClient.on("offline", () => {
   console.log(`Client is offline`);
 });
 
-MQTTClient.on('reconnect', () => {
-  console.log('reconnecting to the broker ....');
+MQTTClient.on("reconnect", () => {
+  console.log("reconnecting to the broker ....");
 });
 
-MQTTClient.on('message', (topic, message, packet) => {
-  if (topic.toString() != 'test') {
-    const messageJSON = JSON.parse(message.toString('utf-8'));
+MQTTClient.on("message", (topic, message, packet) => {
+  if (topic.toString() != "test") {
+    const messageJSON = JSON.parse(message.toString("utf-8"));
     //TODO build a local buffer
     console.log(messageJSON);
 
     axios
       .post(APIUrl, messageJSON)
       .then((res) => {
-        console.log('success');
+        console.log("success");
       })
       .catch((reason) => {
-        console.error('error!');
+        console.error("error!");
         console.log(reason.response?.data);
       });
   }
